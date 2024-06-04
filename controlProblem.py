added state bounds and ICs, changed cost, CO computation fix, fixed plotting code

controlProblem.py
@@ -11,11 +11,21 @@ np.random.seed(0)
 
 #define constants
 #do these bounds vary from patient to patient?
-u0_lb = 0.62
-u0_ub = 1.0
-u1_lb = 81
-u1_ub = 99
-x_init = np.zeros(4)
+#note: x0 = pau, x1 = pal, x2 = pvu, x3 = pvl, u0 = Raup, u1 = Q
+x0_lb = 50
+x0_ub = 100
+x1_lb = 50
+x1_ub = 115
+x2_lb = 0.3
+x2_ub = 3.5
+x3_lb = 1
+x3_ub = 25
+u0_lb = 0.2
+u0_ub = 1.1
+u1_lb = 0
+u1_ub = 200
+x_init = np.array([(x0_lb+x0_ub)/2, (x1_lb+x1_ub)/2, (x2_lb+x2_ub)/2, (x3_lb+x3_ub)/2])
+# x_init = np.zeros(4)
 # RCVals = (1.7, 0.26, 51, 4.3, 0.15, 7.1, 0.028) #does this vary from patient to patient?
 # patientNum = 12726
 
@@ -75,11 +85,12 @@ def solveNOC(paud, RCVals, Qd, h):
         # np.concatenate([np.inf*np.ones(N+1), np.inf*np.ones(N+1), np.inf*np.ones(N+1), np.inf*np.ones(N+1), u0_ub*np.ones(N), u1_ub*np.ones(N)])
         # np.concatenate([None, None, None, None, u0_lb*np.ones(N), u1_lb*np.ones(N)]),
         # np.concatenate([None, None, None, None, u0_ub*np.ones(N), u1_ub*np.ones(N)])
-        np.concatenate([none_arr, none_arr, none_arr, none_arr, u0_lb*np.ones(N), u1_lb*np.ones(N)]),
-        np.concatenate([none_arr, none_arr, none_arr, none_arr, u0_ub*np.ones(N), u1_ub*np.ones(N)])
+        np.concatenate([x0_lb*np.ones(N+1), x1_lb*np.ones(N+1), x2_lb*np.ones(N+1), x3_lb*np.ones(N+1), u0_lb*np.ones(N), u1_lb*np.ones(N)]),
+        np.concatenate([x0_ub*np.ones(N+1), x1_ub*np.ones(N+1), x2_ub*np.ones(N+1), x3_ub*np.ones(N+1), u0_ub*np.ones(N), u1_ub*np.ones(N)])
     )
 
-    cost = lambda z: 100*((get_x0(z)[-1]-paud[-1])/paud[-1])**2 + np.sum(np.square((get_x0(z)[:-1]-paud[:-1])/paud[:-1]))+np.sum(np.square((get_u1(z)[:]-Qd)/Qd)) + np.sum(np.square(get_u0(z)[:]))
+    # cost = lambda z: 100*((get_x0(z)[-1]-paud[-1])/paud[-1])**2 + np.sum(np.square((get_x0(z)[:-1]-paud[:-1])/paud[:-1]))+np.sum(np.square((get_u1(z)[:]-Qd)/Qd)) + np.sum(np.square(get_u0(z)[:]))
+    cost = lambda z: np.sum(np.square((get_x0(z)[:]-paud[:])/paud[:]))+ np.sum(np.square((get_u1(z)[:]-Qd)/Qd)) + 0.01 * np.sum(np.square(get_u0(z)[:]))
 
     def constraints(z):
         f_evaluated = f(np.array([get_x0(z)[:-1], get_x1(z)[:-1], get_x2(z)[:-1], get_x3(z)[:-1]]), np.array([get_u0(z), get_u1(z)]), RCVals)
@@ -89,15 +100,16 @@ def solveNOC(paud, RCVals, Qd, h):
             get_x1(z)[1:] - get_x1(z)[:-1] - h*(f_evaluated[1]),
             get_x2(z)[1:] - get_x2(z)[:-1] - h*(f_evaluated[2]),
             get_x3(z)[1:] - get_x3(z)[:-1] - h*(f_evaluated[3]),
-            #initial conditions
-            get_x0(z)[0:1] - x_init[0],
-            get_x1(z)[0:1] - x_init[1],
-            get_x2(z)[0:1] - x_init[2],
-            get_x3(z)[0:1] - x_init[3],
+
+            #initial conditions (?)
+            # get_x0(z)[0:1] - x_init[0],
+            # get_x1(z)[0:1] - x_init[1],
+            # get_x2(z)[0:1] - x_init[2],
+            # get_x3(z)[0:1] - x_init[3],
         ])
     
     #initial guess for iteration
-    z0 = np.concatenate([np.zeros(N+1), np.zeros(N+1), np.zeros(N+1), np.zeros(N+1), np.zeros(N), np.zeros(N)])
+    z0 = np.concatenate([np.zeros(N+1)+68.0, np.zeros(N+1)+67.0, np.zeros(N+1)+3.5, np.zeros(N+1)+3.75, np.zeros(N)+0.81, np.zeros(N)+90.0])
     result = minimize(cost, 
                       z0, 
                       bounds = bounds, 
@@ -105,7 +117,11 @@ def solveNOC(paud, RCVals, Qd, h):
                           'type': 'eq',
                           'fun': constraints
                       },
-                      options = {'maxiter': 10})
+                      options={
+                          'maxiter': 100,
+                          'disp': True
+                      }
+                    )
     
     verbose = True
     if verbose:

utils.py
@@ -1,12 +1,25 @@
 import numpy as np
+import scipy as sp
 
 def computeCO(weight, height, sex):
     bsa = np.sqrt(weight * height / 3600)
     if sex == 'F':
-        Vtot = (3.47 * bsa - 1.954) / 1000
+        Vtot = (3.47 * bsa - 1.954)*1000
     else:
-        Vtot = (3.47 * bsa - 1.229) / 1000
+        Vtot = (3.47 * bsa - 1.229)*1000
         # Vtot = (2.63 * bsa + 0.146) / 1000
     CO = Vtot / 60
 
     return CO
+
+def computeMAP(bpSignal):
+    # find systolic (high pressure) peaks
+    sysPksIdces = sp.signal.find_peaks(bpSignal, width=30)[0].astype(int) # come back and fix this
+
+    map = np.zeros(sysPksIdces.size-1)
+    for i in range(sysPksIdces.size-1):
+        systolicBp = bpSignal[sysPksIdces[i]]
+        diastolicBp = np.min(bpSignal[sysPksIdces[i]:sysPksIdces[i+1]])
+        map[i] = diastolicBp + (systolicBp - diastolicBp)/ 3.0
+    
+    return (map, sysPksIdces[:-1])